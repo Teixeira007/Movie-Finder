6b3de401f10af28cf271da78533ca1857067b6bd
teixeira007 <viniciusteixeiravini16@gmail.com>
2023-04-15 22:39:44 -0300

    init commit consumindo a api filmes - popular

---
+++ js/script.js
@@ -0,0 +1,33 @@
+const api_key = 'df11c6ebfaff4552cd4ceb4187f105fb';
+
+
+async function popular(){
+    const data = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${api_key}&page=1`)
+    const movies = await data.json()
+    console.log(movies);
+
+    const container = document.getElementById('container')
+    movies.results.forEach(element => {
+        const containerMovie = document.createElement('div')
+        containerMovie.className = 'containerMovie'
+
+        const link = document.createElement('a')
+        link.href = 'google.com'
+
+        const title = document.createElement('span')
+        title.className = 'titleMovie'
+        title.textContent = element.title
+
+        const poster = document.createElement('img')
+        poster.className = 'posterMovie'
+        poster.src = `https://image.tmdb.org/t/p/w500${element.poster_path}`
+
+        link.append(poster)
+
+        containerMovie.append(link)
+        containerMovie.append(title)
+        container.append(containerMovie)
+    });
+
+}
+popular()
