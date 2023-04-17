e9bbb37f23f1b71a720b5d9854164c1c290926e1
teixeira007 <viniciusteixeiravini16@gmail.com>
2023-04-17 00:05:26 -0300

    Button Close Search

--- js/script.js
+++ js/script.js
@@ -160,15 +160,37 @@ topRanqued()
 async function searchMenu(){
     const searchIcon = document.querySelector('.search-icon');
     const searchBox = document.querySelector('.search-box');
+    const imgIconSearch = document.querySelector('.img-icon-search');
     
-    searchIcon.addEventListener('mouseover', event => {
+    searchIcon.addEventListener('click', event => {
         searchBox.style.display = "block"
+        searchIcon.style.display = "none"
+        const iconsMenuRight = document.querySelector('.icons-menu-right')
+        
+        const closeIcon = document.createElement('button')
+        closeIcon.className = "closeIcon"
+
+        const imgClose = document.createElement('img')
+        imgClose.src = '../icon/close.png'
+
+        closeIcon.append(imgClose)
+        iconsMenuRight.append(closeIcon)
+        // console.log(iconsMenuRight.style.flexDirection);
+        iconsMenuRight.style.flexDirection = 'row-reverse'
+
+
+        // const closeIcon = document.querySelector('.newClose');
+        // console.log(closeIcon);
+        closeIcon.addEventListener('click', event => {
+        // console.log("close");
+            searchBox.style.display = "none"
+            closeIcon.style.display = "none"
+            searchIcon.style.display = "block"
+            iconsMenuRight.style.flexDirection = 'row'
+        })
     })
 
-    searchIcon.addEventListener('click', event => {
-        console.log("Buscando");
-        const nameSearch = searchBox.value
-    })
+    
 
     searchBox.addEventListener('keydown', function(event){
         if(event.key == "Enter"){
