8455421152db02c9cc98a37c2b70b265bbfbd4d7
teixeira007 <viniciusteixeiravini16@gmail.com>
2023-04-16 22:34:18 -0300

    Movie topRanqued, Movies Now, Movies Trending

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ async function popular(){
     const movies = await data.json()
     console.log(movies);
 
-    const listMovie = document.getElementById('listMovie')
+    const listMovie = document.querySelector('#listMoviePopular')
     movies.results.forEach(element => {
         const containerMovie = document.createElement('li')
         containerMovie.className = 'containerMovie'
@@ -35,13 +35,126 @@ async function popular(){
         listMovie.append(containerMovie)
     });
 
-    const buttonRight = document.createElement('button')
-    buttonRight.value= ">"
-    buttonRight.className = "buttonRight"
-    listMovie.append(buttonRight)
+    
 
 }
+
+
+
+async function cartaz(){
+    const data = await fetch(`https://api.themoviedb.org/3/movie/now_playing?api_key=${api_key}&page=1`)
+    const movies = await data.json()
+    console.log(movies);
+
+    const listMovie = document.querySelector('#listMovieCartaz')
+    movies.results.forEach(element => {
+        const containerMovie = document.createElement('li')
+        containerMovie.className = 'containerMovie'
+
+        const link = document.createElement('a')
+        link.href = 'google.com'
+        link.className = 'link-poster'
+
+        const linkTitle = document.createElement('a')
+        linkTitle.href = '#'
+        linkTitle.className = 'link-title'
+
+        const title = document.createElement('span')
+        title.className = 'titleMovie'
+        title.textContent = element.title
+
+        const poster = document.createElement('img')
+        poster.className = 'posterMovie'
+        poster.src = `https://image.tmdb.org/t/p/w500${element.poster_path}`
+
+        link.append(poster)
+        linkTitle.append(title)
+
+        containerMovie.append(link)
+        containerMovie.append(linkTitle)
+        listMovie.append(containerMovie)
+    });
+
+}
+
+async function trending(){
+    const data = await fetch(`https://api.themoviedb.org/3/trending/all/day?api_key=${api_key}`)
+    const movies = await data.json()
+    console.log(movies);
+
+    const listMovie = document.querySelector('#listMovieTrending')
+    movies.results.forEach(element => {
+        const containerMovie = document.createElement('li')
+        containerMovie.className = 'containerMovie'
+
+        const link = document.createElement('a')
+        link.href = 'google.com'
+        link.className = 'link-poster'
+
+        const linkTitle = document.createElement('a')
+        linkTitle.href = '#'
+        linkTitle.className = 'link-title'
+
+        const title = document.createElement('span')
+        title.className = 'titleMovie'
+
+        element.title ? title.textContent = element.title :title.textContent = element.name
+
+        const poster = document.createElement('img')
+        poster.className = 'posterMovie'
+        poster.src = `https://image.tmdb.org/t/p/w500${element.poster_path}`
+
+        link.append(poster)
+        linkTitle.append(title)
+
+        containerMovie.append(link)
+        containerMovie.append(linkTitle)
+        listMovie.append(containerMovie)
+    });
+}
+
+
+async function topRanqued(){
+    const data = await fetch(`https://api.themoviedb.org/3/movie/top_rated?api_key=${api_key}&page=1`)
+    const movies = await data.json()
+    console.log(movies);
+
+    const listMovie = document.querySelector('#listMovieTop')
+    movies.results.forEach(element => {
+        const containerMovie = document.createElement('li')
+        containerMovie.className = 'containerMovie'
+
+        const link = document.createElement('a')
+        link.href = 'google.com'
+        link.className = 'link-poster'
+
+        const linkTitle = document.createElement('a')
+        linkTitle.href = '#'
+        linkTitle.className = 'link-title'
+
+        const title = document.createElement('span')
+        title.className = 'titleMovie'
+
+        element.title ? title.textContent = element.title :title.textContent = element.name
+
+        const poster = document.createElement('img')
+        poster.className = 'posterMovie'
+        poster.src = `https://image.tmdb.org/t/p/w500${element.poster_path}`
+
+        link.append(poster)
+        linkTitle.append(title)
+
+        containerMovie.append(link)
+        containerMovie.append(linkTitle)
+        listMovie.append(containerMovie)
+    });
+}
+
 popular()
+cartaz()
+trending()
+topRanqued()
+
 
 
 async function searchMenu(){
@@ -68,19 +181,43 @@ async function searchMenu(){
 
 
 async function carousel(){
-    const buttonRight = document.querySelector('.buttonRight');
-    const buttonLeft = document.querySelector('.buttonLeft');
+    const buttonRight = document.querySelectorAll('.buttonRight');
+    const buttonLeft = document.querySelectorAll('.buttonLeft');
 
     const carousel = document.querySelector('.carousel');
-
-    console.log(carousel.scrollleft);
-
-    buttonRight.addEventListener('click', event =>{
+    const carouselNow = document.querySelector('#carousel-now')
+    const carouselCartaz = document.querySelector('#carousel-cartaz')
+    const carouselTop = document.querySelector('#carousel-top')
+    // console.log(carousel.scrollleft);
+    
+    buttonRight[0].addEventListener('click', event =>{
         carousel.scrollLeft += 1137;
     })
 
-    buttonLeft.addEventListener('click', event =>{
+    buttonLeft[0].addEventListener('click', event =>{
         carousel.scrollLeft -=1137;
+    })
+    buttonRight[1].addEventListener('click', event =>{
+        carouselNow.scrollLeft += 1137;
+    })
+
+    buttonLeft[1].addEventListener('click', event =>{
+        carouselNow.scrollLeft -=1137;
+    })
+    buttonRight[2].addEventListener('click', event =>{
+        carouselCartaz.scrollLeft += 1137;
+    })
+
+    buttonLeft[2].addEventListener('click', event =>{
+        carouselCartaz.scrollLeft -=1137;
+    })
+
+    buttonRight[3].addEventListener('click', event =>{
+        carouselTop.scrollLeft += 1137;
+    })
+
+    buttonLeft[3].addEventListener('click', event =>{
+        carouselTop.scrollLeft -=1137;
     })
 }
 carousel()
