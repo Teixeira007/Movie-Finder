4f2d3f3fcf98197e13a96f3e1f91171197461e02
teixeira007 <viniciusteixeiravini16@gmail.com>
2023-04-22 00:37:56 -0300

    Sistema de cadastro e login funcionando com o banco mysql

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const session = require('express-session');
+const bodyParser = require('body-parser');
+const http = require('http');
+const mysql = require('mysql2')
 
 const port = 3000;
 let path = require('path');
@@ -14,9 +17,28 @@ app.use('/css', express.static(path.join(__dirname, 'css')))
 app.use('/js', express.static(path.join(__dirname, 'js')))
 app.use('/img', express.static(path.join(__dirname, 'img')))
 app.use('/icon', express.static(path.join(__dirname, 'icon')))
-
-
 app.set('views', path.join(__dirname, '/views'));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+// Crie uma conexão com o banco de dados MySQL
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'admin',
+  database: 'moviefinder',
+});
+
+connection.connect((err) => {
+    if (err) {
+      console.error('Erro de conexão:', err);
+      return;
+    }
+    console.log('Conexão bem-sucedida ao banco de dados MySQL.');
+});
+  
+
 
 
 app.get('/', function(req, res) {
@@ -35,6 +57,28 @@ app.get('/cadastrar', (req, res) => {
     res.sendFile(path.join(__dirname, '/views/TelaLoginCadastro.html'));
 })
 
+app.post('/login', (req, res) => {
+    const { email, password } = req.body;
+    connection.query('SELECT * FROM users WHERE email = ? AND password = ?', [email, password], (error, results) => {
+      if (error) throw error;
+      if (results.length > 0) {
+        // Se as credenciais estiverem corretas, armazena o ID do usuário na sessão
+        req.session.userId = results[0].id;
+        res.redirect('/');
+      } else {
+        res.redirect('/login?id=login&error=Credenciais inválidas.');
+      }
+    });
+  });
+  
+  app.post('/cadastro', (req, res) => {
+    const { name, email, password } = req.body;
+    connection.query('INSERT INTO users (name, email, password) VALUES (?, ?, ?)', [name, email, password], (error, results) => {
+      if (error) throw error;
+      res.redirect('/login?id=login');
+    });
+  });
+
 app.listen(port, ()=>{
-    console.log('servidor rodando');
+    console.log('servidor rodando na porta:', port);
 })
